ec5856bf1cd8e97b87b0f66a0db811f4e7280c13
Philipp Benner <benner@molgen.mpg.de>
2017-12-15 18:15:23 +0100

    2017/12/15-18:15:23 (Linux sirius unknown)

---
+++ statistics/matrixDistribution/shapeHmm_data.go
@@ -0,0 +1,55 @@
+/* Copyright (C) 2017 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package matrixDistribution
+
+/* -------------------------------------------------------------------------- */
+
+//import   "fmt"
+
+import . "github.com/pbenner/ngstat/statistics"
+
+import . "github.com/pbenner/autodiff"
+
+/* -------------------------------------------------------------------------- */
+
+type ShapeHmmDataRecord struct {
+  Edist []MatrixDistribution
+  X       Matrix
+}
+
+func (obj ShapeHmmDataRecord) MapIndex(k int) int {
+  return k
+}
+
+func (obj ShapeHmmDataRecord) GetN() int {
+  n, _ := obj.X.Dims()
+  return n
+}
+
+func (obj ShapeHmmDataRecord) LogPdf(r Scalar, c, k int) error {
+  n, m := obj.Edist[c].Dims()
+
+  i := k - m/2
+  j := k - m/2 + m
+
+  if _, l := obj.X.Dims(); i > 0 && j < l {
+    return obj.Edist[c].LogPdf(r, obj.X.Slice(0, n, i, j))
+  } else {
+    r.SetValue(0.0)
+    return nil
+  }
+}
