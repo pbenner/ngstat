f1263ebb7337364201e12a22e8c7d6624eb69558
Philipp Benner <benner@molgen.mpg.de>
2018-07-25 12:52:10 +0200

    2018/07/25-12:52:10 (Linux zoophobie.molgen.mpg.de unknown)

--- track/segmentation.go
+++ track/segmentation.go
@@ -36,28 +36,7 @@ import   "github.com/pbenner/autodiff/statistics/generic"
 
 /* -------------------------------------------------------------------------- */
 
-var segmentationRgbChart []string
-
-func init() {
-  segmentationRgbChart = []string{
-    "128,0,0",
-    "34,139,34",
-    "30,144,255",
-    "210,105,30",
-    "255,215,0",
-    "85,107,47",
-    "124,255,60",
-    "128,128,0",
-    "255,69,0",
-    "238,232,170",
-    "102,205,0",
-    "184,134,11",
-    "139,90,0",
-    "255,97,3",
-    "128,0,0"}
-}
-
-func generateNColors(n int) []string {
+func getNColors(n int) []string {
   // number of levels for each color
   k := int(math.Cbrt(float64(n)))+1
   // delta for each color
@@ -72,89 +51,10 @@ func generateNColors(n int) []string {
   return s
 }
 
-func getNColors(n int) []string {
-  if n <= len(segmentationRgbChart) {
-    return segmentationRgbChart
-  } else {
-    return generateNColors(n)
-  }
-}
-
 /* -------------------------------------------------------------------------- */
 
-func exportTrackSegmentation(granges GRanges, bedFilename, name, description string, compress bool) error {
-  buffer := new(bytes.Buffer)
-
-  w := bufio.NewWriter(buffer)
-  if _, err := fmt.Fprintf(w, "track name=\"%s\" description=\"%s\" visibility=1 itemRgb=\"On\"\n", name, description); err != nil {
-    return err
-  }
-  if err := granges.WriteBed9(w); err != nil {
-    return err
-  }
-  w.Flush()
-
-  if compress {
-    b := new(bytes.Buffer)
-    w := gzip.NewWriter(b)
-    io.Copy(w, buffer)
-    w.Close()
-    buffer = b
-  }
-  return ioutil.WriteFile(bedFilename, buffer.Bytes(), 0666)
-}
-
 func ExportTrackSegmentation(config SessionConfig, track Track, bedFilename, bedName, bedDescription string, compress bool, stateNames, rgbChart []string) error {
-  r, err := GenericTrack{track}.GRanges("state"); if err != nil {
-    return err
-  }
-  state      := r.GetMetaFloat("state")
-  name       := make([]string, len(state))
-  score      := make([]int,    len(state))
-  thickStart := make([]int,    len(state))
-  thickEnd   := make([]int,    len(state))
-  itemRgb    := make([]string, len(state))
-
-  if rgbChart == nil || stateNames == nil {
-    sMax := 0
-    for i := 0; i < r.Length(); i++ {
-      if s := int(state[i]); s > sMax {
-        sMax = s
-      }
-    }
-    if rgbChart == nil {
-      rgbChart = getNColors(sMax+1)
-    }
-    if stateNames == nil {
-      stateNames = make([]string, sMax+1)
-      for i := 0; i < len(stateNames); i++ {
-        stateNames[i] = fmt.Sprintf("s%d", i)
-      }
-    }
-  }
-  for i := 0; i < r.Length(); i++ {
-    s := int(state[i])
-    if s < 0 || math.Floor(state[i]) != state[i] {
-      return fmt.Errorf("invalid state `%f' at `%s:%d-%d", state[i], r.Seqnames[i], r.Ranges[i].From, r.Ranges[i].To)
-    }
-    if s >= len(rgbChart) {
-      return fmt.Errorf("rgbChart has not enough colors")
-    }
-    if s >= len(stateNames) {
-      return fmt.Errorf("insufficient number of state names")
-    }
-    name      [i] = stateNames[s]
-    score     [i] = 0
-    thickStart[i] = r.Ranges[i].From
-    thickEnd  [i] = r.Ranges[i].To
-    itemRgb   [i] = rgbChart[s]
-  }
-  r.AddMeta("name",       name)
-  r.AddMeta("thickStart", thickStart)
-  r.AddMeta("thickEnd",   thickEnd)
-  r.AddMeta("itemRgb",    itemRgb)
-  // write result to file
-  return exportTrackSegmentation(r, bedFilename, bedName, bedDescription, compress)
+  return (GenericTrack{track}).ExportSegmentation(bedFilename, bedName, bedDescription, compress, stateNames, rgbChart)
 }
 
 /* -------------------------------------------------------------------------- */
@@ -238,12 +138,31 @@ func ImportTrackSegmentation(config SessionConfig, bedFilename string, genome Ge
 
 /* -------------------------------------------------------------------------- */
 
+func exportTrackSegmentation(granges GRanges, bedFilename, name, description string, compress bool) error {
+  buffer := new(bytes.Buffer)
+
+  w := bufio.NewWriter(buffer)
+  if _, err := fmt.Fprintf(w, "track name=\"%s\" description=\"%s\" visibility=1 itemRgb=\"On\"\n", name, description); err != nil {
+    return err
+  }
+  if err := granges.WriteBed9(w); err != nil {
+    return err
+  }
+  w.Flush()
+
+  if compress {
+    b := new(bytes.Buffer)
+    w := gzip.NewWriter(b)
+    io.Copy(w, buffer)
+    w.Close()
+    buffer = b
+  }
+  return ioutil.WriteFile(bedFilename, buffer.Bytes(), 0666)
+}
+
 func ExportHierarchicalTrackSegmentation(config SessionConfig, track Track, bedFilename, bedName, bedDescription string, compress bool, stateNames, rgbChart []string, tree generic.HmmNode, level int) error {
   r, err := GenericTrack{track}.GRanges("state"); if err != nil {
     return err
-  }
-  if rgbChart == nil {
-    rgbChart = segmentationRgbChart
   }
   rgbMap := make(map[int]int)
   rgbCnt := 0
@@ -252,18 +171,12 @@ func ExportHierarchicalTrackSegmentation(config SessionConfig, track Track, bedF
     if d == level {
       if node.Children == nil {
         for i := 0; i < len(node.Children); i++ {
-          if node.States[i] > len(rgbChart) {
-            return nil, fmt.Errorf("rgbChart has not enough colors")
-          }
           for k := node.States[0]; k < node.States[1]; k++ {
             rgbMap[k] = rgbCnt
           }
           rgbCnt++
         }
       } else {
-        if len(node.Children) > len(rgbChart) {
-          return nil, fmt.Errorf("rgbChart has not enough colors")
-        }
         for i := 0; i < len(node.Children); i++ {
           if r, err := f(node.Children[i], d+1); err != nil {
             return nil, err
@@ -305,10 +218,12 @@ func ExportHierarchicalTrackSegmentation(config SessionConfig, track Track, bedF
     for i := 0; i < len(stateNames); i++ {
       stateNames[i] = fmt.Sprintf("s%d", i)
     }
-  } else {
-    if len(rgbMap) != len(stateNames) {
-      return fmt.Errorf("invalid number of state names")
-    }
+  }
+  if len(rgbChart) == 0 {
+    rgbChart = getNColors(len(rgbMap))
+  }
+  if len(rgbMap) != len(stateNames) {
+    return fmt.Errorf("invalid number of state names")
   }
   // get states for the lowest level
   state_old  := r.GetMetaFloat("state"); if len(state_old) == 0 {
